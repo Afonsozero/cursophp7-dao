dbcfdc8af5bee40cc4f3e937cb27ac095bae896a
Afonso <afonsolacerda.lacerda@gmail.com>
2019-04-09 19:00:33 -0300

    Atualizacao

--- class/usuario.php
+++ class/usuario.php
@@ -8,34 +8,34 @@ class Usuario {
 	private $dtcadastro;
 
 	public function getIdusuario(){
-		return $this->idusuario;
+        return $this->idusuario;
 	}
 
-	public function setIdusuario(){
+	public function setIdusuario($value){
 		$this->idusuario = $value;
 	}
 
-		public function getDeslogin(){
+	public function getDeslogin(){
 		return $this->deslogin;
 	}
 
-	public function setDeslogin(){
+	public function setDeslogin($value){
 		$this->deslogin = $value;
 	}
 
-		public function getDessenha(){
+	public function getDessenha(){
 		return $this->dessenha;
 	}
 
-	public function setDessenha(){
+	public function setDessenha($value){
 		$this->dessenha = $value;
 	}
 
-		public function getDtcadastro(){
+	public function getDtcadastro(){
 		return $this->dtcadastro;
 	}
 
-	public function setDtcadastro(){
+	public function setDtcadastro($value){
 		$this->dtcadastro = $value;
 	}
 
@@ -43,28 +43,29 @@ class Usuario {
 
 		$sql = new Sql();
 
-		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID ", array(
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+            ":ID"=>$id 
+
 		));
-    if (count($results) > 0) {
 
-    	$row = $results[0];
+		if (count($results) > 0) {
 
-		$this->setIdusuario($row['idusuario']);
-		$this->setDeslogin($row['deslogin']);
-		$this->setDessenha($row['dessenha']);
-		$this->setDtcadastro(new DateTime($row['dtcadastro']));
-    }
+			$row = $results[0];
 
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->getDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}
 	}
 
 	public function __toString(){
 
-     return json_encode(array(
-     	"idusuario"=>$this->getIdusuario(),
-        "deslogin"=>$this->getDeslogin(),
-        "dessenha"=>$this->getDessenha(),
-        "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
-    ));
+		return json_encode(array(
+			"idusuario"=>$this->getIdusuario(),
+			"deslogin"=>$this->getDeslogin(),
+			"dessenha"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+		));
 	}
 }
 
