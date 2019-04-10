6782c4330e31eeac534dd855f478a0df3e2963d2
Afonso <afonsolacerda.lacerda@gmail.com>
2019-04-09 22:10:06 -0300

    Atualizacao

--- index.php
+++ index.php
@@ -2,16 +2,35 @@
 
 require_once("config.php");
 
+
+/*Carrega um usuário
 $root = new Usuario();
 
 $root->loadbyId(3);
 
 echo $root;
+*/
 
 //$sql = new Sql();
 
 //$usuarios = $sql->select("SELECT * FROm tb_usuarios");
 
 //echo json_encode($usuarios);
+
+//carrega uma lista de usuários
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usaário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "root");
+
+echo $usuario;
+
 
 ?>
